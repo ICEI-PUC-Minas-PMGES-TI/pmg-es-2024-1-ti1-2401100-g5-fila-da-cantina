1e6181a8de73c72019524670ff3987de0e897496
foolish8932 <contasuch123@gmail.com>
2024-06-25 12:38:33 -0300

    feat: finished cart logic

--- codigo/src/assets/js/index.js
+++ codigo/src/assets/js/index.js
@@ -1,3 +1,64 @@
+function formatName(name) {
+  const removeTracos = name.replace('-', ' ');
+
+  const words = removeTracos.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+
+  const formatted = words.join(' ');
+
+  return formatted;
+}
+
+function refreshCart() {
+  const cart = window.localStorage.getItem('cart');
+  const cartItems = JSON.parse(cart);
+
+  cartItems.map((item) => {
+    $(`#${item.name}`).remove();
+
+    $newdiv = $(
+      `<div class="bg-gray-200 border-b border-blue-400 rounded-t-md" id="${
+        item.name
+      }"><div class="flex justify-between p-4"><div class="flex flex-col"><span class="text-xl">${formatName(
+        item.name
+      )}</span><span class="text-gray-600 font-semibold">Unidades: ${parseInt(
+        item.units
+      )}x</span></div><span class="text-xl text-blue-400">R$ ${
+        item.price * item.units
+      }</span></div></div>`
+    );
+
+    $('#cart').append($newdiv);
+  });
+}
+
+function refreshCartOverview() {
+  const cart = window.localStorage.getItem('cart');
+  const cartItems = JSON.parse(cart);
+  var totalPrice = 0;
+  var totalItems = 0;
+
+  if (cartItems) {
+    cartItems.map((item) => {
+      totalItems += item.units;
+    });
+    cartItems.map((item) => {
+      if (item.units === 1) {
+        totalPrice += item.price;
+      } else {
+        const total = item.units * item.price;
+        totalPrice += total;
+      }
+    });
+
+    $('#cart-overview').text(`Carrinho (${totalItems})`);
+    $('#cart-overview-mobile').text(`Carrinho (${totalItems})`);
+    $('#cart-total').text(`R$ ${totalPrice}`);
+  }
+}
+
 $(document).ready(async function () {
   const cookies = getCookie(document.cookie);
 
@@ -21,15 +82,58 @@ $(document).ready(async function () {
           `<span class="text-white">${user.name.charAt(0).toUpperCase()}</span>`
         ).appendTo('#avatar');
 
-        $('#add-item-to-cart').on('click', function (e) {
-          console.log($(this).attr('itemprop'))
-        });
-
         const cart = window.localStorage.getItem('cart');
 
         if (cart) {
           const cartItems = JSON.parse(cart);
+
+          cartItems.map((item) => {
+            $newdiv = $(
+              `<div class="bg-gray-200 border-b border-blue-400 rounded-t-md" id="${
+                item.name
+              }"><div class="flex justify-between p-4"><div class="flex flex-col"><span class="text-xl">${formatName(
+                item.name
+              )}</span><span class="text-gray-600 font-semibold">Unidades: ${parseInt(
+                item.units
+              )}x</span></div><span class="text-xl text-blue-400">R$ ${
+                item.price * item.units
+              }</span></div></div>`
+            );
+
+            $('#cart').append($newdiv);
+          });
+          refreshCartOverview();
         }
+
+        $('#add-item-to-cart').on('click', function (e) {
+          const currentItem = JSON.parse($(this).attr('itemprop'));
+
+          const cart = window.localStorage.getItem('cart');
+
+          if (cart) {
+            const cartItems = JSON.parse(cart);
+            cartItems.map((item) => {
+              if (item.name === currentItem.itemprop.name) {
+                item.units++;
+                window.localStorage.setItem('cart', JSON.stringify(cartItems));
+
+                refreshCart();
+                refreshCartOverview();
+              }
+            });
+          } else {
+            var obj = [
+              {
+                name: currentItem.itemprop.name,
+                units: 1,
+                price: currentItem.itemprop.price,
+              },
+            ];
+            window.localStorage.setItem('cart', JSON.stringify(obj));
+            refreshCart();
+            refreshCartOverview();
+          }
+        });
 
         $('#logout').on('click', function () {
           eraseCookies();
