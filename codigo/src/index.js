1ca0a740bac7db5d7252ddc53bca39c2587cba19
foolish8932 <contasuch123@gmail.com>
2024-06-30 22:49:27 -0300

    fix: environment port variable

--- codigo/src/index.js
+++ codigo/src/index.js
@@ -132,6 +132,6 @@ app.get('/payment/create', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server rodando em http://localhost:3000');
 });
